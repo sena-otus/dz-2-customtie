5b60871d1055e2f0a1fa5171fa88d206fe507a13
sena@s73.work <Sergey>
2023-11-13 21:41:17 +0100

    final version

--- customtie.cpp
+++ customtie.cpp
@@ -12,10 +12,39 @@ auto getPerson() {
                          );
 }
 
+
+    // that works:
+template<typename... Args>
+std::tuple<Args&...> customtie1(Args&... args) noexcept
+{
+  return std::tuple<Args&...>(args...);
+}
+
+
+  // that works too:
+template<typename... Args>
+std::tuple<Args&...> customtie2(Args&... args) noexcept
+{
+  return {args...};
+}
+
+// NOLINTNEXTLINE(hicpp-named-parameter,readability-named-parameter)
 int main(int, char *[]) {
-  std::string name, secondName, department;
-  std::size_t age;
-  std::tie(name, secondName, age, department) = getPerson();
+  std::string name;
+  std::string secondName;
+  std::string department;
+  std::size_t age{0};
+  customtie1(name, secondName, age, department) = getPerson();
+  assert(name == "Petia");
+  assert(secondName == "Ivanoff");
+  assert(age == 23);
+  assert(department == "Sale");
+
+  name.clear();
+  secondName.clear();
+  department.clear();
+  age = 0;
+  customtie2(name, secondName, age, department) = getPerson();
   assert(name == "Petia");
   assert(secondName == "Ivanoff");
   assert(age == 23);
